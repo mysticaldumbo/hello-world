1f6692944ae7e041b5fd0ba88bc57d11d9d9bde5
copper <bananaananab69@gmail.com>
2024-05-19 12:41:54 -0500

    man fuck jinja

---
+++ langs/ReactTS.tsx
@@ -0,0 +1,9 @@
+// TSX is an extension of TypeScript commonly used with React for writing type-safe JSX code.
+
+import React from 'react';
+
+const HelloWorld: React.FC = () => {
+  return <h1>Hello, World!</h1>;
+}
+
+export default HelloWorld;
