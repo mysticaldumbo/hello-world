89eb3f543755d1e3a16d2d855ff38cc730ca3023
copper <bananaananab69@gmail.com>
2024-05-19 12:17:09 -0500

    more h

---
+++ langs/Haskell.hs
@@ -0,0 +1,3 @@
+-- Haskell is a purely functional programming language known for its strong static typing and lazy evaluation.
+main :: IO ()
+main = putStrLn "Hello, World!"
---
+++ langs/Haxe.hx
@@ -0,0 +1,6 @@
+// Haxe is a cross-platform programming language and compiler.
+class Main {
+    static function main() {
+        trace("Hello, World!");
+    }
+}
